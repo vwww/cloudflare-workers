77a1b274fb9e2058d602d70bb3f7c4a18903d275
Victor <theonlypwner@users.noreply.github.com>
2023-06-25 11:58:57 -0600

    redirectloop: add delay

--- redirectloop/index.js
+++ redirectloop/index.js
@@ -1,5 +1,10 @@
-addEventListener("fetch", event => {
+addEventListener('fetch', (event) => event.respondWith(handler(event)))
+
+async function handler (event) {
   const url = new URL(event.request.url)
   url.pathname = `/${+url.pathname.slice(1) + 1}`
-  event.respondWith(Response.redirect(url))
-})
+
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  return Response.redirect(url.toString())
+}
