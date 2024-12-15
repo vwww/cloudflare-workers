b5a991c47bd6992237583517638b6c186ec33de9
Victor <theonlypwner@users.noreply.github.com>
2024-12-14 19:59:38 -0700

    redirectloop: add robots.txt

--- redirectloop/index.js
+++ redirectloop/index.js
@@ -1,7 +1,18 @@
 addEventListener('fetch', (event) => event.respondWith(handler(event)))
 
+const ROBOTS_TEXT = `User-agent: *
+Allow: /$
+Allow: /1
+Allow: /2
+Disallow: /`
+
 async function handler (event) {
   const url = new URL(event.request.url)
+
+  if (url.pathname == '/robots.txt') {
+    return new Response(ROBOTS_TEXT)
+  }
+
   url.pathname = `/${+url.pathname.slice(1) + 1}`
 
   await new Promise((resolve) => setTimeout(resolve, 1000))
